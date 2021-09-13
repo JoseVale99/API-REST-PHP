cb4c6972b5c22bfed890529407fcd399b9d82653
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-13 00:03:37 -0500

    add method return data for id

--- models/Category.php
+++ models/Category.php
@@ -2,9 +2,9 @@
 
 class Category extends Connection{
 
-
+// method SELECT information for status equals at 1
     public function getCategory(){
-        // method for connection in db
+        
         $connect = parent::connect();
         parent::set_names();
 
@@ -13,6 +13,20 @@ class Category extends Connection{
 
         return $result = $sql->fetchAll(PDO::FETCH_ASSOC);
     }
+    //  method for return category for ID.
+    public function getCategoryID($cat_id){
+        $connect = parent::connect();
+        parent::set_names();
+
+        $sql = $connect->prepare(
+            "SELECT * FROM category WHERE estatus=1 AND cat_id =?");
+        $sql->execute();
+
+        return $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+
 }
 
 
